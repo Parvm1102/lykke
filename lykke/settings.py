127860f6903a974600bc5a976c317f0d7fd3ce50
Parvm1102 <parvmittal31757@gmail.com>
2025-08-21 22:58:05 +0530

    gitignore

--- lykke/settings.py
+++ lykke/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-$e&&mt6%+azuaj6&lbzeick$w()!&!me)cfuh3b==wwbl$2-2h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lykke.onrender.com']
 
 
 # Application definition
